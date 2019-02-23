24abc277d4ba157d4867a20860d4de8627857945
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2019-02-23 22:54:54 +0530

    minor fix

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import promise from "redux-promise";
+import { BrowserRouter as Router } from "react-router-dom";
+import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import promise from "redux-promise";
+import "semantic-ui-css/semantic.min.css";
 import App from "./App";
 import "./index.css";
-import registerServiceWorker from "./registerServiceWorker";
-import "semantic-ui-css/semantic.min.css";
 import rootReducer from "./reducers/index";
+import registerServiceWorker from "./registerServiceWorker";
 
 const store = createStore(
-	rootReducer,
-	composeWithDevTools(applyMiddleware(promise))
+  rootReducer,
+  composeWithDevTools(applyMiddleware(promise))
 );
 
 ReactDOM.render(
-	<Router>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</Router>,
-	document.getElementById("root")
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
+  document.getElementById("root")
 );
 registerServiceWorker();
